7bda3e485e948fd74b6eaf49ef42092702ed4fe2
Yang Tae-ho <yangbuk84@gmail.com>
2013-03-22 12:34:53 +0900

    php Test

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+ini_set("display_errors", "1");
+$host = 'http://ec2-23-23-52-147.compute-1.amazonaws.com';
+$username = 'root';
+$password = '111111';
+$dbname = 'today_fortune';
+$link = mysql_connect($host, $username, $password);
+if (!$link) {
+  die('접속실패 : ' . mysql_error());
+} else {
+    echo "접속성공<br />";
+}
+$db_selected = mysql_select_db($dbname, $link);
+if (!$db_selected) {
+   die ('Can\'t use foo : ' . mysql_error());
+} else {
+    echo "DB선택성공<br />";
+}
+?>
